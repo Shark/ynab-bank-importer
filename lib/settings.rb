a751e7ab10d02361afdc4f1730cf7b28c12f5ffa
Felix Seidel <felix@seidel.me>
2018-08-24 12:45:21 +0200

    Allow settings to be passed in as ENV variable

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,9 +1,14 @@
+require 'base64'
+
 # Reads the settings from the yaml file
 class Settings
   def self.all
     @settings ||= begin
       if ENV['ENV'] == 'test'
         YAML.load_file(File.join('.', 'spec/config.test.yml'))
+      elsif ENV['YNAB_BANK_IMPORTER_CONFIG']
+        settings_string = Base64.strict_decode64(ENV['YNAB_BANK_IMPORTER_CONFIG'])
+        YAML.load(settings_string)
       else
         YAML.load_file(File.join('.', 'config.yml'))
       end
